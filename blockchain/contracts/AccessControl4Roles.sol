3d11f0087b80d4f458cf2a502b58027241a70e3a
GiammiGreg <giammi.greganti@gmail.com>
2026-03-25 01:24:11 +0100

    prova autorizzazione

--- blockchain/contracts/AccessControl4Roles.sol
+++ blockchain/contracts/AccessControl4Roles.sol
@@ -1,11 +1,10 @@
 
-
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.0;
 import "./Contract_bn.sol";
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract AccessControl3Roles is Contract_bn, AccessControl {
+contract AccessControl4Roles is Contract_bn, AccessControl {
     // Definizione dei due ruoli
     bytes32 public constant Studente = keccak256("Studente");
     bytes32 public constant EnteCert = keccak256("EnteCert");
@@ -17,14 +16,36 @@ contract AccessControl3Roles is Contract_bn, AccessControl {
     error CallerNotAzienda(address caller);
     error CallerNotAdmin(address caller);
 
-    constructor(address entecert, address studente, address azienda, address admin) {
+    //-----------------------------------------------------------------------------------------
+    //Qua è dove posso scegliere chi impersonare
+    constructor(address entecert, address azienda, address studente, address admin) {
         // Assegna i ruoli agli indirizzi specificati
-        _grantRole(EnteCert, entecert);
-        _grantRole(Studente, studente);
-        _grantRole(Azienda, azienda);
-        _grantRole(Admin, admin); 
-    }
 
+
+        //address IOEnteCert = (entecert == address(0)) ? msg.sender : entecert;
+        //_grantRole(EnteCert, IOEnteCert); // Assegno il ruolo di entecert a chi distribuisce il contratto
+
+        //address IOstudente = (studente == address(0)) ? msg.sender : studente;
+        //_grantRole(Studente, IOstudente); // Assegno il ruolo di studente a chi distribuisce il contratto
+
+        address IOAdmin = (admin == address(0)) ? msg.sender : admin;
+        _grantRole(Admin, IOAdmin); // Assegno il ruolo di admin a chi distribuisce il contratto
+
+        //address IOazienda = (azienda == address(0)) ? msg.sender : azienda;
+        //_grantRole(Azienda, IOazienda); // Assegno il ruolo di azienda a chi distribuisce il contratto
+
+
+
+        _grantRole(EnteCert, entecert);
+
+        _grantRole(Studente, studente); 
+
+        //_grantRole(Admin, admin);
+
+        _grantRole(Azienda, azienda);
+         
+    }
+  //-----------------------------------------------------------------------------------------
 
 
     function Access_set_apriorProb(
@@ -93,13 +114,14 @@ contract AccessControl3Roles is Contract_bn, AccessControl {
 
 */
     // Funzione riservata agli Admin
-    function permissions_Admin(address _contractbnAddress, // Indirizzo del contratto BN
+    function permissions_Admin(address _contractbnAddress, 
             uint16 _BasiProg, 
             uint16 _ProgPy,
             Contract_bn.IDCERTProb calldata _IDCERTprob, 
             Contract_bn.CorsoPyProb calldata _CorsoPyprob,
             Contract_bn.FondInfoProb calldata _FondInfoprob,
-            Contract_bn.IngSoftProb calldata _IngSoftprob) public {
+            Contract_bn.IngSoftProb calldata _IngSoftprob) external {
+        //msg.sender è l'indirizzo di chi chiama la funzione, se non ha il ruolo di Admin, viene generato un errore
         if (!hasRole(Admin, msg.sender)) {
             revert CallerNotAdmin(msg.sender);
         }
--- blockchain/scripts/deploy_and_txnconAC.py
+++ blockchain/scripts/deploy_and_txnconAC.py
@@ -1,6 +1,9 @@
-from brownie import accounts, AccessControl3Roles, Contract_bn
+from brownie import accounts, AccessControl4Roles, Contract_bn
 import json
 import os
+
+#brownie run scripts/deploy_and_txnconAC.py --network ganache-gui
+
 
 def load_json(filename):
     path = os.path.join(os.path.dirname(__file__), '..', '..', 'data', 'json', filename)
@@ -9,17 +12,40 @@ def load_json(filename):
 
 def deploy_and_set_values():
 
-    account = accounts.add(os.environ.get("PRIVATE_KEY"))
-    
+#-------------------------------------------------------------------------------------
+#QUI "account" è l'account che distribuisce il contratto, e che ha i permessi di Admin in questo esempio
+
+#Se vuoi simulare un altro ruolo, ad esempio: EnteCert, basta che commenti la riga "account_EnteCert"
+#perchè l'account per simulare il ruolo è "account" e decommenti la riga "account_Admin"
+
+    account = accounts.add(os.environ.get("PRIVATE_KEY")) #IO
+    account_EnteCert = accounts.add(os.environ.get("PRIVATE_KEY_EnteCert"))
+    account_Azienda = accounts.add(os.environ.get("PRIVATE_KEY_Azienda"))
+    account_Studente = accounts.add(os.environ.get("PRIVATE_KEY_Studente")) 
+    #account_Admin = accounts.add(os.environ.get("PRIVATE_KEY_Admin"))
+#-------------------------------------------------------------------------------------
+
+
     print(f"Sto eseguendo il deploy con l'account: {account}...")
+
     contract_bn = Contract_bn.deploy({"from": account})
-    accesscontrol = AccessControl3Roles.deploy(
-    account.address,  # entecert
-    account.address,  # studente
-    account.address,  # azienda
-    account.address,  # admin
-    {"from": account}
-)
+
+#--------------------------------------------------------------------------------------
+    #N.B. qua bisogna rispettare l'ordine degli ingressi del constructor che definisce i ruoli in AccessControl4Roles.sol
+    #prima ci va l'account che simula EnteCert, poi Azienda, poi Studente e infine Admin
+    accesscontrol = AccessControl4Roles.deploy(
+        account_EnteCert.address,  # entecert 
+        account_Azienda.address,  # azienda
+        account_Studente.address,  # studente
+
+        account.address, #IO (in questo caso sto simulando l'Admin, quindi "account.address" va messo alla fine,
+        #se avessi simulato tipo EnteCert, avrei messo "account.address" come primo ingresso, commentato
+        #l'ingresso "account_EnteCert.address" e decommentato "account_Admin.address"
+
+        #account_Admin.address,  # admin
+    {"from": account})
+#--------------------------------------------------------------------------------------
+
     Fattore = 1000
 
     # ── Carica i valori dai JSON ──────────────────────────────
